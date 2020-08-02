94a019759fb1ebf77aa9a52fb9679cf134ec325b
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-02 11:44:15 +0530

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Strapi Stripe`,
+    description: `creating a website with frontend as Gatsby, strapi as backend and stripe for payments.`,
+    author: `@sayan_m_`,
   },
   plugins: [
     {

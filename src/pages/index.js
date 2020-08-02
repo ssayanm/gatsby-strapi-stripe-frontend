966f5ef5c60cbe4158ed1d3f5c4306381fa7b5a6
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-08-02 11:00:28 +0530

    cleaned up pages and fetched products data from strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Image from "gatsby-image"
 import SEO from "../components/seo"
+import "../style.css"
 
-const IndexPage = () => (
+import { fromProductSlugToUrl } from "../utils/products"
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {data.allStrapiProducts.nodes.map(product => {
+      return (
+        <Link to={fromProductSlugToUrl(product.slug)}>
+          <div key={product.id} className="home">
+            <div>
+              <Image
+                fluid={product.thumbnail.childImageSharp.fluid}
+                className="home-img"
+              />
+              <p>
+                {product.name} | ${product.price}
+              </p>
+            </div>
+          </div>
+        </Link>
+      )
+    })}
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allStrapiProducts {
+      nodes {
+        id
+        content
+        name
+        price
+        slug
+        thumbnail {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
